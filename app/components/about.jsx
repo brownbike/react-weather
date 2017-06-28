9adf756b1afc46c77025a47af34a31e1fbfc0265
Scott McKeon <scottmckeon@gmail.com>
2017-06-28 15:35:15 -0700

    Add copy to about page and style

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,8 +3,16 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Hello</p>
+			<h1 className="text-center">About</h1>
+			<p>A weather app that uses React</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/">React</a>
+				</li>
+				<li>
+					<a href="https://openweathermap.org/">Openweathermap</a>
+				</li>
+			</ul>
 		</div>
 	)
 };
